cfe3d406691630fcc22733e20c39a215c8fca0bc
yesenia180405 <yes.ale1804@gmail.com>
2026-01-29 20:55:05 +0100

    feat: Crear componente section, con los elementos titulo, subtitulo, barra de busqueda y filtro (todavia sin funcionalidad)

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { Section } from './section/section';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header],
-  template: `<app-header></app-header>`
+  imports: [Header,Section],
+  template: `
+  <app-header></app-header>
+  <app-section></app-section>
+  `
 })
+
+
 export class Home {}
