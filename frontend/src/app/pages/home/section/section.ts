0000ce3aef675049f2e472586b9f5914350c89d8
yesenia180405 <yes.ale1804@gmail.com>
2026-01-30 20:49:26 +0100

    feat: Creación de los componentes footer, section y links-section para el home público

---
+++ frontend/src/app/pages/home/section/section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  imports: [],
+  templateUrl: './section.html',
+  styleUrl: './section.css',
+})
+export class Section {
+
+}
+  
