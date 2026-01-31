e1c8693502f817e1e39f9319ffed62391b7e4b95
Jhonttt <juanatahonadev@gmail.com>
2026-01-31 16:44:49 +0100

    feat: crear modelos (Proyecto y añadir rol y activo en el fillable de User) y seeders de dichos modelos. Configurar el DatabaseSeeder para que se puedan ejectuar

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('rol', ['admin']); // Solo ADMIN por ahora
+            $table->enum('rol', ['admin', "usu"]);
             $table->boolean('activo')->default(true);
             $table->timestamps();
 });
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -6,20 +6,19 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+use Database\Seeders\UserSeeder;
+use Database\Seeders\ProyectoSeeder;
+
+class DatabaseSeeder extends Seeder {
     use WithoutModelEvents;
 
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(UserSeeder::class);
+        $this->call(ProyectoSeeder::class);
     }
 }
