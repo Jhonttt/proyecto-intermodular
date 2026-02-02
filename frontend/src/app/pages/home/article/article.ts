a6fa0f643a99f97327175d4412529f60191ecdc1
Jhonttt <juanatahonadev@gmail.com>
2026-02-02 16:18:48 +0100

    feat: crear componente article reutilizable, con imagen, titulo, descripción etiquetas y año

--- frontend/src/app/pages/home/article/article.ts
+++ frontend/src/app/pages/home/article/article.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './article.html',
   styleUrl: './article.css',
 })
 export class Article {
-
+  @Input() imagen!: string;
+  @Input() titulo!: string;
+  @Input() anio!: string | number;
+  @Input() descripcion!: string;
+  @Input() tecnologias: string[] = [];
 }
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -6,6 +6,12 @@ import { Article } from './article/article';
   selector: 'app-home',
   standalone: true,
   imports: [Article],
-  template: `<app-article></app-article>`
+  template: `<app-article
+    imagen="images/prueba.webp"
+    titulo="Sistema de Gestión Universitaria"
+    anio="2019"
+    descripcion="Sistema para la administración académica y administrativa."
+    [tecnologias]="['Angular', 'React', 'Javascript']"
+  ></app-article> `,
 })
 export class Home {}
