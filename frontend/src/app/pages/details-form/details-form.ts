b0661fbfa8219de0306d138af6da147e2e2a9b8d
alexstmsal <alex.stmsal@gmail.com>
2026-02-02 18:10:21 +0100

    feat: Agregar el componente details-form a la estructura del proyecto

---
+++ frontend/src/app/pages/details-form/details-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-form',
+  imports: [],
+  templateUrl: './details-form.html',
+  styleUrl: './details-form.css',
+})
+export class DetailsForm {
+
+}
