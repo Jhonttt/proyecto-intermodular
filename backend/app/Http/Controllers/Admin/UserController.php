330541360c871f26e5617368bc613b9fcb73ef6b
Iván <ivanibaiherbosa@gmail.com>
2026-02-03 20:55:38 +0100

    feat(admin-users): rutas admin y controlador base

--- backend/app/Http/Controllers/Admin/UserController.php
+++ backend/app/Http/Controllers/Admin/UserController.php
@@ -6,5 +6,18 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.usuarios.index');
+    }
+
+    public function create()
+    {
+        return view('admin.usuarios.create');
+    }
+
+    public function edit($id)
+    {
+        return view('admin.usuarios.edit');
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/usuarios', [UserController::class, 'index'])->name('admin.usuarios.index');
+    Route::get('/usuarios/create', [UserController::class, 'create'])->name('admin.usuarios.create');
+    Route::get('/usuarios/{id}/edit', [UserController::class, 'edit'])->name('admin.usuarios.edit');
 });
