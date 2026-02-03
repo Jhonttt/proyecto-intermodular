d45f6f9483cab2cb7b57b7845311b5b4e1031dbb
Alex <alex.stmsal@gmail.com>
2026-02-03 18:00:28 +0100

    feat: Añadir código en el componente details-form. Tanto tscomo html

--- frontend/src/app/pages/details-form/details-form.ts
+++ frontend/src/app/pages/details-form/details-form.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details-form',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './details-form.html',
   styleUrl: './details-form.css',
 })
-export class DetailsForm {
+export class DetailsForm implements OnInit{
+
+  projectID!: number;
+
+  constructor(private route: ActivatedRoute) {}
+
+   ngOnInit(): void {
+    // Leer el parámetro :id de la URL
+    this.projectID = Number(this.route.snapshot.paramMap.get('id'));
+  }
 
 }
