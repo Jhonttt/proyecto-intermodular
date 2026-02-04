275c289dc01eafd00a9f8b9f72844e7a9a0f560a
Alex <alex.stmsal@gmail.com>
2026-02-04 17:41:24 +0100

    feat: Terminado el panel de detalles de proyecto

--- frontend/src/app/pages/details-form/details-form.ts
+++ frontend/src/app/pages/details-form/details-form.ts
@@ -1,21 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './details-form.html',
-  styleUrl: './details-form.css',
+  styleUrls: ['./details-form.css'],
 })
-export class DetailsForm implements OnInit{
+export class DetailsForm implements OnInit {
+  projectId!: number;
 
-  projectID!: number;
+  //Estados de la pantalla
+  loading: boolean = true;
+  error: boolean = false;
+
+  //Pruebas
+  project: {
+    title: string;
+    autores: string | string[];
+    descripcion: string;
+    curso: string;
+    etiquetas: string;
+  } | null = null;
 
   constructor(private route: ActivatedRoute) {}
 
-   ngOnInit(): void {
-    // Leer el parámetro :id de la URL
-    this.projectID = Number(this.route.snapshot.paramMap.get('id'));
+  ngOnInit(): void {
+    this.projectId = Number(this.route.snapshot.paramMap.get('id'));
+
+    //Carga simulada del backend
+
+    try {
+      this.project = {
+        title: 'Desarrollo web en Laravel',
+        autores: ['Felipe V', 'Gustavo Sánchez'],
+        descripcion: 'Texto de ejemplo para muestra de práctica',
+        curso: '2º DAW',
+        etiquetas: 'Laravel, PHP, Angular',
+      };
+
+      this.loading = false;
+    } catch (e) {
+      this.error = true;
+      this.loading = false;
+    }
   }
 
+  //Para el HTML (evita usar instanceof en template)
+  get autoresEsArray(): boolean {
+    return Array.isArray(this.project?.autores);
+  }
 }
