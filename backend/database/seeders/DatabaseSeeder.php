c0888f7694cd40f2f6d4be6c7e2674f60c8f1152
yesenia180405 <yes.ale1804@gmail.com>
2026-02-05 20:20:09 +0100

    Revert "Feature/details form"

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -6,19 +6,20 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use Database\Seeders\UserSeeder;
-use Database\Seeders\ProyectoSeeder;
-
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
     use WithoutModelEvents;
 
     /**
      * Seed the application's database.
      */
-    public function run(): void {
+    public function run(): void
+    {
         // User::factory(10)->create();
 
-        $this->call(UserSeeder::class);
-        $this->call(ProyectoSeeder::class);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/login', [AuthController::class, 'login'])->name('login');
