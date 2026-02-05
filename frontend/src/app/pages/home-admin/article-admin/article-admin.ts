2ef46963e9e611112cbc3510af8cab3c24e4bb14
carolina <carolgithub12@gmail.com>
2026-02-05 18:16:22 +0100

    feat(home-admin): creacion de estructura y componentes

---
+++ frontend/src/app/pages/home-admin/article-admin/article-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-admin',
+  imports: [],
+  templateUrl: './article-admin.html',
+  styleUrl: './article-admin.css',
+})
+export class ArticleAdmin {
+
+}
