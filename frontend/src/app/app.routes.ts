a8c130ba5907bb49158dc3d83ae5e3e4257223fe
carolina <carolgithub12@gmail.com>
2026-02-05 21:34:21 +0100

    feat(header-admin): componente con opcion de cerrar sesion del admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Home } from './pages/home/home'; 
+import { HomeAdmin } from './pages/home-admin/home-admin';
 
 import { DetailsForm } from './pages/details-form/details-form';
 
 export const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: 'home', component: Home },
+  { path: 'admin', component: HomeAdmin },
   { path: "details-form/:id", component: DetailsForm },
 ];
--- frontend/src/app/pages/home-admin/article-admin/article-admin.ts
+++ frontend/src/app/pages/home-admin/article-admin/article-admin.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-admin',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './article-admin.html',
   styleUrl: './article-admin.css',
 })
 export class ArticleAdmin {
-
+  //@Input() imagen!: string;
+  @Input() titulo!: string;
+  @Input() anio!: string | number;
+  @Input() descripcion!: string;
+  @Input() tecnologias: string[] = [];
 }
