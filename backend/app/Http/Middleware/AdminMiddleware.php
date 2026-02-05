f70096f311cd8ab22e77ed3de869887871071c2d
Jhonttt <juanatahonadev@gmail.com>
2026-02-05 17:49:11 +0100

    feat: terminar todo el middleware

--- backend/app/Http/Middleware/AdminMiddleware.php
+++ backend/app/Http/Middleware/AdminMiddleware.php
@@ -6,16 +6,14 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdminMiddleware
-{
+class AdminMiddleware {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {
-         if (!$request->user() || !$request->user()->isAdmin()) {
+    public function handle(Request $request, Closure $next): Response {
+        if (!$request->user() || !$request->user()->isAdmin()) {
             return response()->json([
                 'message' => 'Acceso denegado. Solo administradores.'
             ], 403);
