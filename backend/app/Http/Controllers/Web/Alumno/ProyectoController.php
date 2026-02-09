36e41fc899c847e0fd5fff22d96a44034f9f0870
Paula <pau.saez08@gmail.com>
2026-02-09 16:44:45 +0100

    Creación de la lógica de ProyectoController

--- backend/app/Http/Controllers/Web/Alumno/ProyectoController.php
+++ backend/app/Http/Controllers/Web/Alumno/ProyectoController.php
@@ -7,5 +7,37 @@ use Illuminate\Http\Request;
 
 class ProyectoController extends Controller
 {
-    //
+     public function store(Request $request)
+    {
+      // Validación
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'resumen' => 'required|string',
+            'descripción' => 'required|string',
+            'curso' => 'required|string|max:255',
+            'alumnos' => 'required|string',
+            'video_url' => 'nullable|url',
+            'archivo' => 'required|file|max:30720', // 30 MB (KB)
+        ]);
+
+        //Guardar archivo
+        $rutaArchivo = $request->file('archivo')->store('proyectos');
+
+        //Guardar en BD
+        $proyecto = Proyecto::create([
+            'nombre' => $request->nombre,
+            'resumen' => $request->resumen,
+            'descripción' => $request->descripción,
+            'curso' => $request->curso,
+            'alumnos' => $request->alumnos,
+            'video_url' => $request->video_url,
+            'checked' => false,           //false por defecto
+            'observaciones' => null,
+        ]);
+
+        return response()->json([
+            'message' => 'Proyecto guardado correctamente',
+            'data'    => $proyecto
+        ], 201);
+    }
 }
