eedc17280752f69b61837b85fa94afce3a488c42
Juan <juanatahonadev@gmail.com>
2026-02-10 19:30:29 +0100

    Change user role enum from 'alumno' to 'usu'

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->enum('rol', ['admin', 'alumno']);
+            $table->enum('rol', ['admin', 'usu']);
             $table->boolean('activo')->default(true);
             $table->timestamps();
         });
