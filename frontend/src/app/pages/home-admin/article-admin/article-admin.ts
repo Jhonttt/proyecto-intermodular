9e6af5120abc7e721c62317e5e68406cd1dc79e1
Renzito <renzojrzegarra@gmail.com>
2026-02-16 23:30:43 +0100

    Revert "feat(home-admin): agregar paginación y nuevas columnas de estado y botones"
    This reverts commit 53e66949736b9b2fa3b8406f3bf03eeb7784233c.

--- frontend/src/app/pages/home-admin/article-admin/article-admin.ts
+++ frontend/src/app/pages/home-admin/article-admin/article-admin.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,15 +17,4 @@ export class ArticleAdmin {
   @Input() anio!: string | number;
   @Input() descripcion!: string;
   @Input() tecnologias: string[] = [];
-  @Input() acciones: string[] = [];
-  @Input() estado!: 'validado' | 'pendiente';
-  
-  @Output() validar = new EventEmitter<void>();
-
-  onValidar() {
-    this.validar.emit();
-  }
-
-  onEditar() {}
-  onEliminar() {}
 }
