fe5476e144cd99c97fa26f2f7b034c1ffe3ea63b
yesenia180405 <yes.ale1804@gmail.com>
2026-02-20 16:53:06 +0100

    fix: layout con opciones dinamicas

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './pages/layout/navbar/navbar';
 import { Footer } from './pages/layout/footer/footer';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+
+  ngOnInit(): void {
+    // 🔹 Solo recargar la página la primera vez que entra
+    if (!sessionStorage.getItem('reloaded')) {
+      sessionStorage.setItem('reloaded', 'true');
+      window.location.href = window.location.href; // 🔹 Recarga completa
+    }
+  }
 }
